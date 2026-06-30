b51fa90c57da5f04e6ef5aef9d1bf434df78d201
Syed Taneer <stamegy@gmail.com>
2026-06-30 19:52:37 +0500

    Ports issues fixed for docker

--- taskManager-Frontend/src/api/tasks.ts
+++ taskManager-Frontend/src/api/tasks.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/tasks/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/tasks`;
 
 export interface Task {
   id: number;
